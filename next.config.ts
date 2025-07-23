d39d3b4ba9e48c26c56107a243369735f5da8c25
ederramosh <eder.ramosh@gmail.com>
2025-07-23 22:10:32 +0000

    setting next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,17 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+      },
+      {
+        protocol: "https",
+        hostname: "mosaic.scdn.co",
+      },
+    ],
   },
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "i.scdn.co",
-    },
-    {
-      protocol: "https",
-      hostname: "mosaic.scdn.co",
-    },
-  ],
 };
 
 export default nextConfig;
