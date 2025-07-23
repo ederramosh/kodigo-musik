be6b649f94f564b901f994a4154f9abc233a2b4d
ederramosh <eder.ramosh@gmail.com>
2025-07-23 20:53:43 +0000

    fixing enviroment variable, token

--- src/app/api/gettoptracks.service.ts
+++ src/app/api/gettoptracks.service.ts
@@ -1,4 +1,4 @@
-export const token = 'BQC4E1U0oAiVwPjYx-OyLS4erV8C_sywQ-GnddlZwAWrwLp6lzYkRRBqqdTT467-W9oM37I0LN7vXqbNE0Rxf17QexC2BdrIvew-Cp2xtTJ6WSfyosXNKrXnGoOdOLm7tfT1ylBlW9YShiT7PU689oSpiIdFazPuTfU4GVRRs-gKvV008tfY-11-0Vutjlmv_AkJ7Xow2YTVrGzRXFSm6S9CmM3Nk_5jqQ5HoPgWOZf4-0fflbut5OoSvff4rPwWtFHt7WNXcN27ie6Nrg-CEOV-oFjEK7jh1lxPrksV';
+export const token = process.env.NEXT_PUBLIC_SPOTIFY_TOKEN;
 
 async function fetchWebApi(endpoint: string, method: string, body?: Record<string, unknown>) {
   const res = await fetch(`https://api.spotify.com/${endpoint}`, {
